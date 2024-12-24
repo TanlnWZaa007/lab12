4a44e113627ecd43cbe58dc74330bd107fb0809e
TanlnWZaa007 <153242404+TanlnWZaa007@users.noreply.github.com>
2024-12-24 16:25:48 +0700

    Update lab12_4.cpp

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+template<typename T>
+void mySwap(T &, T &);
+
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +34,9 @@ int main(){
 	return 0;
 }
 
+template<typename T>
+void mySwap(T &x, T &y) {
+    T z = x;
+    x = y;
+    y = z;
+}
